fac3094b175f4f394aaed34c48e90f87736be357
Alexander Schrab <alexander.schrab@gmail.com>
2018-02-26 21:40:29 +0100

    Android map is working

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,7 +5,17 @@ import MapView from 'react-native-maps';
 
 export default class Map extends React.Component<{}, {}> {
   render() {
-    return <MapView style={styles.map} />;
+    return (
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={styles.map}
+      />
+    );
   }
 }
 
